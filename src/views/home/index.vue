dca80805d46245382dc8a71bbdd05e9fd36aae7d
诗琳 <2498686798@qq.com>
2023-05-26 19:28:14 +0800

    fix: xxxx

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,11 +1,15 @@
 <template>
   <div class="home card">
     <!-- <img class="home-bg" src="@/assets/images/welcome.png" alt="welcome" /> -->
-  测试
+    <!-- 测试 -->
+
+    <Vue3Lottie :animationData="DogJSON" class="home-bg" />
   </div>
 </template>
 
-<script setup lang="ts" name="home"></script>
+<script setup lang="ts" name="home">
+import DogJSON from "../imagejson/home_bg.json";
+</script>
 
 <style scoped lang="scss">
 @import "./index.scss";
