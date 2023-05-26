d13ee0dbe36827e5c2c5d9c59979e1ffdecfe624
诗琳 <2498686798@qq.com>
2023-05-26 17:31:11 +0800

    测试

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="home card">
-    <img class="home-bg" src="@/assets/images/welcome.png" alt="welcome" />
+    <!-- <img class="home-bg" src="@/assets/images/welcome.png" alt="welcome" /> -->
+  测试
   </div>
 </template>
 
