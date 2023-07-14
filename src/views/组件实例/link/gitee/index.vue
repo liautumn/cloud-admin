43b28f2175d06fe3e2ebae07f6eeeaf0865b274a
autumn <1060590974@qq.com>
2023-07-14 19:40:07 +0800

    去除多余组件

--- src/views/组件实例/link/gitee/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="card content-box"></div>
-</template>
-
-<script setup lang="ts" name="gitee"></script>
-
-<style scoped lang="scss">
-@import "./index.scss";
-</style>
--- src/views/组件实例/link/github/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="card content-box"></div>
-</template>
-
-<script setup lang="ts" name="github"></script>
-
-<style scoped lang="scss">
-@import "./index.scss";
-</style>
