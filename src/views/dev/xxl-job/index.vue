2b8b3e5d80dea07441a028d0026ec63b2d79d3d3
autumn <1060590974@qq.com>
2023-07-24 16:22:26 +0800

    xxl-job

---
+++ src/views/dev/xxl-job/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <iframe src="http://10.144.100.1:8086/xxl-job" class="full-iframe"></iframe>
+</template>
+
+<script setup lang="ts" name="xxl-job"></script>
+
+<style scoped lang="scss">
+.full-iframe {
+  width: 100%;
+  height: 100%;
+}
+</style>
